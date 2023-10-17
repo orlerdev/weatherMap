86b1e119d4ad30a8d6362b13a3120cfb58b30b10
Jared Orler <orlerdev@icloud.com>
2023-10-17 09:28:09 -0500

    update gitignore

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,4 @@
+const Landing = () => {
+	return <div className='flex flex-col w-screen h-screen p-4 gap-x-4'></div>
+}
+export default Landing
---
+++ src/components/landing/Weather.jsx
@@ -0,0 +1,4 @@
+const Weather = () => {
+	return <div className='flex flex-col w-full h-full p-4'></div>
+}
+export default Weather
